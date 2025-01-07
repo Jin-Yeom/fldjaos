c4daa574f412a3ecfa0a23b1d4c522df54b26fd4
jinyeomlee <vpxh12@naver.com>
2025-01-07 17:50:09 +0900

    main 작업중..

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,105 @@
-import {MainWrapper} from './styled';
+import {MainWrapper} from "./styled";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import { arrayUnion, collection, doc, getDoc, getFirestore, updateDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
 
+const firebaseConfig = {
+	apiKey: "AIzaSyAq9_eXkpuWq5rDAS5oWi9vyI_MsgwO4pI",
+	authDomain: "main-32d80.firebaseapp.com",
+	projectId: "main-32d80",
+	storageBucket: "main-32d80.appspot.com",
+	messagingSenderId: "370479894776",
+	appId: "1:370479894776:web:8c37b943c9f50b210a6b0d",
+	measurementId: "G-B4WYQX7L3K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const db = collection(database, "fldjaos");
+const querySnapshot1 = await getDoc(doc(db, "app"));
 
 const Main = () => {
 
-  
+  /**
+   * 시계 UI 생성, 시간을 불러온다.
+   */
+  const ClockBox = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+
+    return <div className="clock-box">
+      <div id="clock">{hours}:{minutes}</div>
+    </div>;
+}
+
+  /**
+   * app box 생성
+   */
+  const AppBox = () => {
+      const appFb = querySnapshot1.data();
+      let html = [];
+      let appArrFb = [];
+
+      for(let i = 0; i < appFb.ID.length; i++) {
+        const data = {
+            CONTENT : appFb.CONTENT[i],
+            ICON : appFb.ICON[i],
+            ID : appFb.ID[i],
+            IMAGE : appFb.IMAGE[i],
+            READY : appFb.READY[i],
+            TITLE : appFb.TITLE[i],
+            URL : appFb.URL[i]
+        };
+
+        appArrFb.push(data);
+      }
+      
+      appArrFb.forEach((item, idx) => {
+          const {ID:id, ICON:icon} = item;
+
+          html.push(<div key={idx} className="app-box" id={"app-"+id} ></div>);
+      })
+
+      return html;
+  };
+
+  /**
+   * 매핑된 앱의 팝업 생성
+   */
+  // const createPopupHTML = () => {
+  //     let html = "";
+
+  //     appArrFb.forEach((item, idx) => {
+  //         const {ID:id, TITLE:title, IMAGE:image, CONTENT:content, READY:ready, URL:url} = item;
+
+  //         html += `<div class="popup" id=${id} style="display: none;">
+  //                     <span class="close-btn">×</span>
+  //                     <h2>${title}</h2>
+  //                     <div class="popContent" style="background-image: url(${image});"></div>
+  //                     <p>${content}</p>
+  //                     ${ready ? `<button class="btn-start" id="btn-start-${id}" onclick="location.href='${url}'">시작하기</button>` : 
+  //                     `<button class="btn-start" id="btn-start-${id}" disabled=true>준비중..</button>`}
+  //                 </div>`;
+  //     })
+
+
+  //     return html;
+  // }
 
   return (
     <MainWrapper>
       <div className='main'>
+        <ClockBox></ClockBox>
         <div className="clock-box"></div>
         <div className="plmi-box">
           <span id="plus" type="button"></span>
           <span id="minus" type="button"></span>
         </div>
         <div className="container">
-          <div className="app-box-container"></div>
+          <div className="app-box-container">
+            <AppBox></AppBox>
+          </div>
         </div>
         <div className="popupContainer" style={{display:'none'}}></div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './components/Main/Main';
 
+const gcm = {};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
