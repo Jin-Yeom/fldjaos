3f91ac19338446191d7f8d943b3a2f4d8c73c5a5
jinyeomlee <vpxh12@naver.com>
2025-01-08 09:45:57 +0900

    firebase 분리 및 api_key 숨김처리

--- src/Hooks/firebase.js
+++ src/Hooks/firebase.js
@@ -1,18 +1,19 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-// import { arrayUnion, collection, doc, getDoc, getFirestore, updateDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';
+import { getFirestore, collection } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyAq9_eXkpuWq5rDAS5oWi9vyI_MsgwO4pI",
-// 	authDomain: "main-32d80.firebaseapp.com",
-// 	projectId: "main-32d80",
-// 	storageBucket: "main-32d80.appspot.com",
-// 	messagingSenderId: "370479894776",
-// 	appId: "1:370479894776:web:8c37b943c9f50b210a6b0d",
-// 	measurementId: "G-B4WYQX7L3K"
-// };
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: "main-32d80.firebaseapp.com",
+	projectId: "main-32d80",
+	storageBucket: "main-32d80.appspot.com",
+	messagingSenderId: "370479894776",
+	appId: "1:370479894776:web:8c37b943c9f50b210a6b0d",
+	measurementId: "G-B4WYQX7L3K"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const database = getFirestore(app);
-// const db = collection(database, "fldjaos");
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const db = collection(database, "fldjaos");
 
+export default db;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,7 @@
 import {MainWrapper} from "./styled";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-import { arrayUnion, collection, doc, getDoc, getFirestore, updateDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+import { arrayUnion, collection, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
+import db from '../../Hooks/firebase';
 
-const firebaseConfig = {
-	apiKey: "AIzaSyAq9_eXkpuWq5rDAS5oWi9vyI_MsgwO4pI",
-	authDomain: "main-32d80.firebaseapp.com",
-	projectId: "main-32d80",
-	storageBucket: "main-32d80.appspot.com",
-	messagingSenderId: "370479894776",
-	appId: "1:370479894776:web:8c37b943c9f50b210a6b0d",
-	measurementId: "G-B4WYQX7L3K"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getFirestore(app);
-const db = collection(database, "fldjaos");
 const querySnapshot1 = await getDoc(doc(db, "app"));
 
 const Main = () => {
