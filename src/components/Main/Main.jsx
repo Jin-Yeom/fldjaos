207ffff8f179c664b182991ffc998f12ea35cd48
jinyeomlee <vpxh12@naver.com>
2025-01-10 13:49:20 +0900

    image 이동

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,36 @@
 import { MainWrapper } from "./styled";
 import { arrayUnion, collection, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
 import db from "../../Hooks/firebase";
+import { useState } from "react";
 
 const querySnapshot1 = await getDoc(doc(db, "app"));
+const appFb = querySnapshot1.data();
+const appData = appFb.ID.map((_, i) => ({
+  CONTENT: appFb.CONTENT[i],
+  ICON: appFb.ICON[i],
+  ID: appFb.ID[i],
+  IMAGE: appFb.IMAGE[i],
+  READY: appFb.READY[i],
+  TITLE: appFb.TITLE[i],
+  URL: appFb.URL[i],
+}));
 
 const Main = () => {
   /**
    * 시계 UI 생성, 시간을 불러온다.
    */
   const Clock = () => {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
+    let now = new Date();
+    const [hours, setHours] = useState(now.getHours().toString().padStart(2, "0"));
+    const [minutes,setMinutes] = useState(now.getMinutes().toString().padStart(2, "0"));
 
+    // 시간 갱신
+    setInterval(() => {
+      now = new Date();
+      setHours(now.getHours().toString().padStart(2, "0"));
+      setMinutes(now.getMinutes().toString().padStart(2, "0"));
+    },300);
+    
     return (
       <section className="clock-box" aria-label="Current Time">
         <time id="clock" aria-live="polite">
@@ -26,27 +44,28 @@ const Main = () => {
    * app box 생성
    */
   const AppList = () => {
-    const appFb = querySnapshot1.data();
-    const appData = appFb.ID.map((_, i) => ({
-      CONTENT: appFb.CONTENT[i],
-      ICON: appFb.ICON[i],
-      ID: appFb.ID[i],
-      IMAGE: appFb.IMAGE[i],
-      READY: appFb.READY[i],
-      TITLE: appFb.TITLE[i],
-      URL: appFb.URL[i],
-    }));
-debugger;
     return (
       <div className="app-box-container" role="list">
         {appData.map(({ ID, ICON }, idx) => (
-          <article key={idx} className="app-box" id={`app-${ID}`} role="listitem" aria-label={`App ${ID}`}>
-            <img src={ICON} alt={`${ID} icon`} />
-          </article>
+          <div key={idx} id={`app-${ID}`} className="app-box" style={ {backgroundImage:`url(${ICON})`} } alt={`${ID} icon`}></div>
         ))}
       </div>
     );
   };
+
+  const appPopup = () => {
+    
+    // return (
+    //   <div class="popup" id=${id} style="display: none;">
+    //     <span class="close-btn">×</span>
+    //     <h2>${title}</h2>
+    //     <div class="popContent" style="background-image: url(${image});"></div>
+    //     <p>${content}</p>
+    //     ${ready ? `<button class="btn-start" id="btn-start-${id}" onclick="location.href='${url}'">시작하기</button>` : 
+    //     `<button class="btn-start" id="btn-start-${id}" disabled=true>준비중..</button>`}
+    //   </div>
+    // );
+  }
 
   return (
     <MainWrapper>
